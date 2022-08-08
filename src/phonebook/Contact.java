9366b8ab6ffe0c1c1e2fc35b9a14382080f0afc2
fox <1987222ruto@gmail.com>
2022-08-08 21:14:14 +0200

    add homework Phonebook task new write

--- src/phonebook/Contact.java
+++ src/phonebook/Contact.java
@@ -1,10 +1,13 @@
 package phonebook;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Contact implements Serializable {
 
-        private Long id;
+    @Serial
+    private static final long serialVersionUID = -7133894735851737395L;
+    private Long id;
         private String name;
 
     public Contact(Long id, String name) {
--- src/phonebook/ObjectPhone.java
+++ src/phonebook/ObjectPhone.java
@@ -1,7 +1,30 @@
 package phonebook;
 
+import java.io.*;
+
 public class ObjectPhone {
     public static void main(String[] args) {
+
+        File fileOutput = new File("/home/fox/IdeaProjects/homework32/example_directory/run/skip/java/output.txt");
+        createNewFileIfNotExist(fileOutput);
+
+
+        try(InputStream inputStream = new FileInputStream("/home/fox/IdeaProjects/homework32/example_directory/run/skip/java/exampleFile.txt");
+        OutputStream outputStream = new FileOutputStream("/home/fox/IdeaProjects/homework32/example_directory/run/skip/java/output.txt")) {
+            byte buffer[] = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            System.out.println(new String(buffer));
+            outputStream.write(buffer);
+
+            Contact contact = new Contact(1L,"Java SE");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(contact);
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createNewFileIfNotExist(File fileOutput) {
 
     }
 }
